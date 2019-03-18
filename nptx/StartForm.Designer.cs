8b8cc7c5298a07b5d4e72e14e3e2fd2b228051ac
Aaron Zuo <zuoyayong@outlook.com>
2019-03-18 22:18:15 +0800

    init

--- nptx/StartForm.Designer.cs
+++ nptx/StartForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace nptx
 {
-    partial class Form1
+    partial class StartForm
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,10 +28,16 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "StartForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- nptx/StartForm.cs
+++ nptx/StartForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace nptx
 {
-    public partial class Form1 : Form
+    public partial class StartForm : Form
     {
-        public Form1()
+        public StartForm()
         {
             InitializeComponent();
         }
